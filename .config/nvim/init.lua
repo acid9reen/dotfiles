e6bf046fc3f55cf175a2d2b5b947e4454d389efb
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2024-02-03 17:22:09 +0300

    Move lazy configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,11 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = 'Open netrw'})
 
 require("set")
 require("remap")
 
-local plugins = require("plugins")
-require("lazy").setup(plugins)
+require("config.lazy")
+
 require("lsp")
 require('Comment').setup()
 require('trim').setup {
