20f4eaf22c52025a1bd2c3ce69efe5bc5092b4b5
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2024-09-08 11:45:14 +0300

    Remove python identation configuration due configuration via treesitter

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -33,12 +33,6 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "100"
 
--- Set up python indentation
-vim.g.python_indent = {
-    open_paren = 'shiftwidth()',
-    closed_paren_align_last_line = false,
-}
-
 -- Some latex stuff
 vim.g.tex_flavor = 'latex'
 
