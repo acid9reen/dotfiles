6ef83a4bacf735ed6fab751648bc9c751d2d38f1
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2025-05-08 13:27:49 +0300

    Update golang sql injections
    idk sometimes it changes

--- .config/nvim/after/queries/go/injections.scm
+++ .config/nvim/after/queries/go/injections.scm
@@ -1,41 +1,49 @@
 ; extends
 
 ; select
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[Ss][Ee][Ll][Ee][Cc][Tt]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[Ss][Ee][Ll][Ee][Cc][Tt]%s*")
     (#set! injection.language "sql"))
 
 ; with
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[wW][iI][tT][hH]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[wW][iI][tT][hH]%s*")
     (#set! injection.language "sql"))
 
 ; insert
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[iI][nN][sS][eE][rR][tT]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[iI][nN][sS][eE][rR][tT]%s*")
     (#set! injection.language "sql"))
 
 ; upsert
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[uU][pP][sS][eE][rR][tT]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[uU][pP][sS][eE][rR][tT]%s*")
     (#set! injection.language "sql"))
 
 ; alter
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[aA][lL][tT][eE][rR]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[aA][lL][tT][eE][rR]%s*")
     (#set! injection.language "sql"))
 
 ; delete
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[dD][eE][lL][eE][tT][eE]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[dD][eE][lL][eE][tT][eE]%s*")
     (#set! injection.language "sql"))
 
 ; update
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[uU][pP][dD][aA][tT][eE]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[uU][pP][dD][aA][tT][eE]%s*")
     (#set! injection.language "sql"))
 
 ; explain
-((raw_string_literal) @injection.content @sql
-    (#lua-match? @sql "^`%s*[eE][xX][pP][lL][aA][iI][nN]%s*")
+(raw_string_literal
+    (raw_string_literal_content) @injection.content @sql
+    (#lua-match? @sql "^%s*[eE][xX][pP][lL][aA][iI][nN]%s*")
     (#set! injection.language "sql"))
