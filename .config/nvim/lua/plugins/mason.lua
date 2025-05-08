e215a4aaaea7559b1a1b7681c2492a67aa2bd601
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2025-05-08 13:35:08 +0300

    Remove lsp config mason due to new mason version

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,20 +1,8 @@
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = true,
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "basedpyright",
-        "gopls",
-      },
-    },
-  },
-  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-      require("mason-lspconfig").setup()
     end,
   },
 }
